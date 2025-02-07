8504bc4f9edf832334c5ed6d79a8e9803335ad27
v1cttorr <murdziawiktor@gmail.com>
2025-02-07 22:41:45 +0100

    Add TaskRoom and Task models

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('room/<int:pk>/', views.room, name='room'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import TaskRoom, Task
+from accounts.models import Profile
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    try:
+        user = Profile.objects.get(user=request.user)
+    except:
+        print('no user')
+
+
+    try:
+        rooms = user.rooms.all()
+    except:
+        rooms = None
+
+    context = {
+        'rooms': rooms,
+    }
+
+    return render(request, 'home.html', context)
+
+@login_required(login_url='login')
+def room(request, pk):
+    user = Profile.objects.get(user=request.user)
+    # room = user.rooms.get(id=pk)
+    room = get_object_or_404(user.rooms, id=pk)
+    context = {
+        'room': room,
+    }
+
+    return render(request, 'room.html', context)
