13e12101829808d7173a7d0c72f49b9dd05ec628
v1cttorr <murdziawiktor@gmail.com>
2025-02-09 13:18:31 +0100

    add subtasks

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import TaskRoom, Task
+from .models import TaskRoom, Task, Subtask
 
 # Register your models here.
 admin.site.register(TaskRoom)
 admin.site.register(Task)
+admin.site.register(Subtask)
